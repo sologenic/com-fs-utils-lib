d604b5f63804f830b94a574d1be710ee9a62ab7e
DylPickle96 <dylan_smartguy@hotmail.com>
2026-02-06 12:49:39 -0800

    feat(role): add partner management and transaction roles
    - Add TX_ACCOUNT_MANAGER and TX_USER_MANAGER roles
    - Add PARTNER_BROKER_ADMIN and PARTNER_BROKER_ACCOUNT_MANAGER roles
    - Add PARTNER_SALES_ADMIN and PARTNER_SALES_ACCOUNT_MANAGER roles for sales channel hierarchy

--- models/build/role/role.d.ts
+++ models/build/role/role.d.ts
@@ -11,6 +11,17 @@ export declare enum Role {
     /** TX_ADMINISTRATOR - Alias for SOLOGENIC_ADMINISTRATOR */
     TX_ADMINISTRATOR = 6,
     KYC_ADMINISTRATOR = 7,
+    /** TX_ACCOUNT_MANAGER - Manages partners and partner assignments */
+    TX_ACCOUNT_MANAGER = 8,
+    /** TX_USER_MANAGER - Manages user/referral aspects */
+    TX_USER_MANAGER = 9,
+    /** PARTNER_BROKER_ADMIN - Can manage partner users for broker partner, and view partner dashboard */
+    PARTNER_BROKER_ADMIN = 10,
+    /** PARTNER_BROKER_ACCOUNT_MANAGER - Day-to-day broker ops, create orgs via wizard, approvals if allowed */
+    PARTNER_BROKER_ACCOUNT_MANAGER = 11,
+    /** PARTNER_SALES_ADMIN - Partner scope hierarchy for sales channel */
+    PARTNER_SALES_ADMIN = 12,
+    PARTNER_SALES_ACCOUNT_MANAGER = 13,
     UNRECOGNIZED = -1
 }
 export declare function roleFromJSON(object: any): Role;
